8eb6825b9528271843afd9fd5c01fe76bba84048
Keith Tang <keithsktang@gmail.com>
2017-09-02 17:28:32 -0400

    add article modle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def new
+  end
+
+  def create
+    render plain: params[:article].inspect
+  end
+
 end

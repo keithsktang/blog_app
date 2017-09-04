082f97fe81537cf1dc552dadab21a3d62de78ee0
Keith Tang <keithsktang@gmail.com>
2017-09-04 12:31:16 -0400

    complete scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def new
-  end
-
-  def create
-    render plain: params[:article].inspect
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'test/index'
+
   get 'welcome/index'
 
   resources :articles
-  
   root "welcome#index"
+  get 'test/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
